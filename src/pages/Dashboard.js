33268e620fe434d2351f3ccbef90cacaeb5290c2
mazkar27 <mazkar27@gmail.com>
2022-03-10 12:50:11 +0700

    add ui chart at dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,8 @@
 import React,{useEffect} from 'react';
 import SmallBox from '../components/small-box/SmallBox';
 import Chart from 'chart.js/auto';
+import HeaderChanger from '@app/components/cardheader/HeaderChanger';
+import {Bar,Line,Pie} from "react-chartjs-2"
 
 
 
@@ -10,47 +12,117 @@ const Dashboard = () => {
         
     },[])
 
+    const colorHex = ['#d93a23','#1960db','#41c358',"#eba111"]
+    const data = {
+        labels:["LTR","SDR","PMR","ORDER REJECTION"],
+      
+        datasets:[{
+            data:[20,48,10,22],
+            backgroundColor: colorHex,
+            display:true,
+            datalabels: {
+                color: '#FFCE56'
+            }
+        }]
+    };
+
+
+
     return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-lg-3 col-6">
-                    <SmallBox
-                        count={0}
-                        title="SDR Done"
-                        type="info"
-                        icon="fas fa-list"
-                        navigateTo="/"
-                    />
+        <>
+            <HeaderChanger title="Dashboard"/>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-lg-3 col-6">
+                        <SmallBox
+                            count={0}
+                            title="SDR Done"
+                            type="info"
+                            icon="fas fa-list"
+                            navigateTo="/" />
+                    </div>
+                    <div className="col-lg-3 col-6">
+                        <SmallBox
+                            count={0}
+                            title="LTR Done"
+                            type="success"
+                            icon="fas fa-list"
+                            navigateTo="/" />
+                    </div>
+                    <div className="col-lg-3 col-6">
+                        <SmallBox
+                            count={0}
+                            title="PMR Done"
+                            type="warning"
+                            icon="fas fa-list"
+                            navigateTo="/" />
+                    </div>
+                    <div className="col-lg-3 col-6">
+                        <SmallBox
+                            count={0}
+                            title="Order Rejection"
+                            type="danger"
+                            icon="fas fa-list"
+                            navigateTo="/" />
+                    </div>
                 </div>
-                <div className="col-lg-3 col-6">
-                    <SmallBox
-                        count={0}
-                        title="LTR Done"
-                        type="success"
-                        icon="fas fa-list"
-                        navigateTo="/"
-                    />
-                </div>
-                <div className="col-lg-3 col-6">
-                    <SmallBox
-                        count={0}
-                        title="PMR Done"
-                        type="warning"
-                        icon="fas fa-list"
-                        navigateTo="/"
-                    />
-                </div>
-                <div className="col-lg-3 col-6">
-                    <SmallBox
-                        count={0}
-                        title="Order Rejection"
-                        type="danger"
-                        icon="fas fa-list"
-                        navigateTo="/"
-                    />
+
+            </div>
+            <div className="container-fluid">
+                <div className='row'>
+                    <div className='col-lg-6 col-md-6'>
+                        <div className='card card-primary'>
+                            <div className='card-header align-middle'>
+                                <h3 className="card-title">Chart</h3>
+                            </div>
+                            <div className="card-body">
+                                <Pie
+                                    data = {data}
+                                    options={{
+                                        responsive:true,
+                                        plugins: {
+                                            legend: {
+                                                position: 'bottom',
+                                            },
+                                            title: {
+                                                display: true,
+                                                text: 'Order Type'
+                                            },
+                                            datalabels: {
+                                               
+                                                display:true,
+                                                align: 'bottom',
+                                               
+                                                
+
+                                            },
+                                            animation: {
+                                                animateScale: true,
+                                                animateRotate: true
+                                            },
+                                           
+                                            
+                                        }}}
+                                    
+                                >
+
+                                </Pie>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-lg-6 col-md-6'>
+                        <div className='card card-primary'>
+                            <div className='card-header align-middle'>
+                                <h3 className="card-title">My Agenda</h3>
+                            </div>
+                            <div className="card-body">
+                                <h1>My Agenda</h1>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
         
     );
 };
