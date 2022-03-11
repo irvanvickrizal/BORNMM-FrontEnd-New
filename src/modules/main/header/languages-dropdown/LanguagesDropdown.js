9b04064e2394e8d6b077bdecef4b001786b1c331
mazkar27 <mazkar27@gmail.com>
2022-03-12 03:36:13 +0700

    add time at navbar

--- src/modules/main/header/languages-dropdown/LanguagesDropdown.js
+++ src/modules/main/header/languages-dropdown/LanguagesDropdown.js
@@ -6,6 +6,7 @@ import React, {useState} from 'react';
 import {useTranslation} from 'react-i18next';
 import {Dropdown} from '@components';
 import { useSelector } from 'react-redux';
+import moment from 'moment';
 
 
 
@@ -15,10 +16,11 @@ const LanguagesDropdown = () => {
     var today = new Date()
     var curHr = today.getHours()
     const dataUser = useSelector(state=>state.auth.user.name)
+    const time = moment().format("hh:mm")
 
     return (
         <div style={{marginTop:'12px',marginRight:'16px'}}>
-            {curHr >= 4 && curHr <= 12 ? (<p style={{fontWeight:"500"}}>{`Good Morning ${dataUser}`}</p>)
+            {curHr >= 4 && curHr <= 12 ? (<p style={{fontWeight:"500"}}>{`Good Morning ${dataUser}              ${time}`}</p>)
                 :curHr <= 18 && curHr >= 12 ? (<p style={{fontWeight:"500"}}>{`Good Afternoon ${dataUser}`}</p>)
                     :curHr >= 18 && curHr <= 24 ? (<p style={{fontWeight:"500"}}>{`Good Evening ${dataUser}`}</p>)
                         :(<><p style={{fontWeight:"500"}}>{`Good Night ${dataUser}`}</p></>)
