e2fdc1db68c9fd7d910d47a498fda3e3eae655a0
Aulian Fathan <aulian26@gmail.com>
2022-04-04 21:35:36 +0700

    change api url to live

--- src/Variables.js
+++ src/Variables.js
@@ -1,6 +1,6 @@
 export const variables = {
     API_URL: 'https://bornxl-api.nsnebast.com/',
-    API_URL_DEMO: 'https://bornxldemo-api.nsnebast.com/',
+    //API_URL_DEMO: 'https://bornxldemo-api.nsnebast.com/',=> for demo
     PHOTO_URL: 'https://localhost:7118/photos/'
 };
 
