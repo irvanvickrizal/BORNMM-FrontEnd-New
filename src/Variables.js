4e2ad5ce3ce703ca9e15710d4f41c7d6c0b15ee5
Aulian Fathan <aulian26@gmail.com>
2022-04-04 14:17:47 +0700

    Add api live parameter

--- src/Variables.js
+++ src/Variables.js
@@ -1,6 +1,8 @@
 export const variables = {
-    API_URL: 'https://bornxldemo-api.nsnebast.com/',
+    API_URL: 'https://bornxl.nsnebast.com/',
+    API_URL_DEMO: 'https://bornxldemo-api.nsnebast.com/',
     PHOTO_URL: 'https://localhost:7118/photos/'
 };
 
-export const API = 'https://bornxldemo-api.nsnebast.com/'
+export const API = ' https://bornxl.nsnebast.com/'
+export const APIDemo = 'https://bornxldemo-api.nsnebast.com/'
