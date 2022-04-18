41ba8208d333167069abbb16083bd17da23d49c0
Aulian Fathan <aulian26@gmail.com>
2022-04-18 09:46:18 +0700

    clear

--- src/Variables.js
+++ src/Variables.js
@@ -3,6 +3,3 @@ export const variables = {
     //API_URL_DEMO: 'https://bornxldemo-api.nsnebast.com/',=> for demo
     PHOTO_URL: 'https://localhost:7118/photos/'
 };
-
-// export const API = ' https://bornxl.nsnebast.com/'
-// export const APIDemo = 'https://bornxldemo-api.nsnebast.com/'
