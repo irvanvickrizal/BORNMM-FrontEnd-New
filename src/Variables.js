ff41383c2673e61dcf87d24b69b1aefc7f9fdade
Aulian Fathan <aulian26@gmail.com>
2022-04-18 09:58:25 +0700

    bugfix

--- src/Variables.js
+++ src/Variables.js
@@ -1,5 +1,5 @@
 export const variables = {
-    API_URL: 'https://bornxldemo-api.nsnebast.com/',
+    API_URL: 'https://bornxl-api.nsnebast.com/',
     //API_URL_DEMO: 'https://bornxldemo-api.nsnebast.com/',=> for demo
     PHOTO_URL: 'https://localhost:7118/photos/'
 };
