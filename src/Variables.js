82eae065b6c3f64bc6d78fc66506ae4301b4ac56
Aulian Fathan <aulian26@gmail.com>
2022-07-07 05:53:27 +0700

    var

--- src/Variables.js
+++ src/Variables.js
@@ -1,5 +1,5 @@
 export const variables = {
-    API_URL: 'https://bornisat-api.nsnebast.com/',
-    //API_URL_DEMO: 'https://bornxldemo-api.nsnebast.com/',=> for demo
+    // API_URL: 'https://bornisat-api.nsnebast.com/',
+    API_URL_DEMO: 'https://bornxldemo-api.nsnebast.com/',
     PHOTO_URL: 'https://localhost:7118/photos/'
 };
