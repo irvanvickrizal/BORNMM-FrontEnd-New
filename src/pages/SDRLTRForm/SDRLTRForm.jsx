6df21b5c06508a14635a04797b0fc3e9b1060c24
Aulian Fathan <aulian26@gmail.com>
2022-07-28 07:24:51 +0700

    bugfix

--- src/pages/SDRLTRForm/SDRLTRForm.jsx
+++ src/pages/SDRLTRForm/SDRLTRForm.jsx
@@ -83,7 +83,7 @@ const SDRLTRForm = (props) => {
     const [selectedPacketType,setSelectedPacketType] = useState('');
     const [selectedSubcon,setSelectedSubcon] = useState('');
     const [selectedSiteCondition,setSelectedSiteCondition] = useState('');
-    const [deliveryDate,setDeliveryDate] = useState(moment(date2, "YYYY-MM-DD").add(3,'d'));
+    const [deliveryDate,setDeliveryDate] = useState(moment(date2, "YYYY-MM-DD").add(2,'d'));
     const [siteAddress,setSiteAddress] = useState('');
     const [selectedTeamCoordinator,setSelectedTeamCoordinator] = useState('');
     const [ddlTeamCoordinator,setDDLTeamCoordinator] = useState([]);
